59f6ac94c0cfd5e62e7f55478408c866c4d262f8
Nathan Chen <113192742+seribical@users.noreply.github.com>
2024-03-18 13:13:50 -0400

    sdfghjui

--- shuitash/src/views/AboutView.vue
+++ shuitash/src/views/AboutView.vue
@@ -1,19 +1,37 @@
 <template>
-  <div>
-
+  <div class="bar">
+  <Bar  :data="data" :options="options" />
   </div>
 </template>
 
-<script>
-export default {
-  setup () {
-    
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import * as chartConfig from '../stores/genquestion'
 
-    return {}
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Bar
+  },
+  data() {
+    return chartConfig
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.bar{
+display: none;
+}
 </style>
